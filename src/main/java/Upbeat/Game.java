0d409afccf7350c6b0d105c4b926a8f812f88ad2
nomon <thanaporn_chan@cmu.ac.th>
2023-04-01 18:41:32 +0700

    delete name1, name2 variables

--- src/main/java/Upbeat/Game.java
+++ src/main/java/Upbeat/Game.java
@@ -22,7 +22,6 @@ import java.util.NoSuchElementException;
 public class Game
 {
     private Player player1, player2;
-    private String nameP1, nameP2;
     private boolean p1Ready, p2Ready, completeSet;
     private Player currentTurn;
     private Player winner;
@@ -40,12 +39,10 @@ public class Game
         if(player1 == null)
         {
             player1 = new Player(name);
-            nameP1 = player1.getName();
         }
-        else if(player2 == null && !name.equals(nameP1))
+        else if(player2 == null)
         {
             player2 = new Player(name);
-            nameP2 = player2.getName();
         }
         return this;
     }
@@ -70,20 +67,16 @@ public class Game
             if(player2 == null)
             {
                 player1 = null;
-                nameP1 = null;
             }
             else
             {
                 player1 = player2;
                 player2 = null;
-                nameP1 = player1.getName();
-                nameP2 = null;
             }
         }
         else
         {
             player2 = null;
-            nameP2 = null;
         }
         return this;
     }
